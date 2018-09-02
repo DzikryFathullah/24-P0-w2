58fa00ee840da15616ec170cea55515566a7b58d
DzikryFathullah <dzizee.fathullah@gmail.com>
2018-09-02 12:41:55 +0700

    upload

---
+++ Exercise4.js
@@ -0,0 +1,31 @@
+var tanggal = 21
+var bulan = 4
+var tahun = 1990
+
+if (tanggal < 1 || tanggal > 31) {
+    console.log('Error !!!, input tanggal salah')
+    tanggal = null
+}
+if (tahun < 1900 || tahun > 2200) {
+    console.log('Error !!!, input tahun diluar jangkauan')
+    tahun = null
+}
+
+switch (bulan) {
+    case 1: { bulan = 'Januari'; break }
+    case 2: { bulan = 'Februari'; break }
+    case 3: { bulan = 'Maret'; break }
+    case 4: { bulan = 'April'; break }
+    case 5: { bulan = 'Mei'; break }
+    case 6: { bulan = 'Juni'; break }
+    case 7: { bulan = 'Juli'; break }
+    case 8: { bulan = 'Agustus'; break }
+    case 9: { bulan = 'September'; break }
+    case 10: { bulan = 'Oktober'; break }
+    case 11: { bulan = 'November'; break }
+    case 12: { bulan = 'Desember'; break }
+    default: { bulan = null; console.log('Error !!!, input bulan salah') }
+}
+
+console.log(tanggal + ' ' + bulan + ' ' + tahun)
+
