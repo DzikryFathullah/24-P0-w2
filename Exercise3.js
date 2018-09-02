b08b7dc3b34b9bb01f8e486f6dff7a7ff1bc784e
DzikryFathullah <dzizee.fathullah@gmail.com>
2018-09-02 11:51:22 +0700

    re upload

---
+++ Exercise3.js
@@ -0,0 +1,16 @@
+var nama = 'dzikry';
+var peran = 'tabib';
+if (nama === '' || peran === '') {
+    console.log('Nama harus diisi!!');
+} else if (peran === 'ksatria') {
+    console.log('Selamat datang di dunia Proxytia, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu');
+} else if (peran === 'tabib') {
+    console.log('Selamat datang di dunia Proxytia, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka');
+} else if (peran === 'penyihir') {
+    console.log('Selamat datang di dunia Proxytia, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+} else {
+    console.log('Hallo ' + nama + ', pilih peranmu untuk memulai game!');
+}
